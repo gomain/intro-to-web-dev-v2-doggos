3828967b24380fcee8ba6871b669084aee51d199
Joe Gomain Hoyes <gomain@gmail.com>
2019-09-17 00:31:09 +0700

    Add breed selection: not working yet

--- script/doggos.js
+++ script/doggos.js
@@ -4,10 +4,25 @@
     Object.prototype.assign = function (obj) {
         return Object.assign(this,obj);
     };
+    Object.prototype.getOwnProperties = function () {
+        return Object.getOwnPropertyNames(this).
+            map(name => ({
+                name: name,
+                value: this[name]
+            }));
+    };
+
+    /* monkey patch HTMLElement */
+    HTMLElement.prototype.setAttr = function (obj) {
+        for (const prop of obj.getOwnProperties()) {
+            this.setAttribute(prop.name,prop.value);
+        }
+        return this;
+    };
 
     const DOGURL = 'https://dog.ceo/api/breeds/image/random';
 
-    const addDogButton = document.querySelector('button#add-random-dog');
+    const addDogButton = document.querySelector('button#add-dog');
     addDogButton.addEventListener('click',() => {
         fetch(DOGURL).
             then(response => response.json()).
@@ -16,7 +31,7 @@
                     appendChild(document.createElement('img').
                                 assign({
                                     src : json.message,
-                                    alt : 'Cute dog!'
+                                    alt : 'Cute doggo!'
                                 }));
                 addDogButton.scrollIntoView();
             });
@@ -24,6 +39,71 @@
     });
     addDogButton.focus();
     
-    console.log(fetch);
+    /* build breed select list */
+    (function () {
+        breedListUrl = 'https://dog.ceo/api/breeds/list/all';
+        fetch(breedListUrl).
+            then(response => response.json()).
+            then(json => {
+                console.log(json);
+                const breedList = json.message;
+                const select = document.querySelector('select#breed-list');
+                select.appendChild(document.createElement('option').
+                                   assign({
+                                       value: 'anyBreed',
+                                       selected: true,
+                                       innerText: 'any doggo'
+                                   }).setAttr({
+                                       optionType: 'any'
+                                   }));
+                for(const breed in breedList) {
+                    if (breedList.hasOwnProperty(breed)){
+                        const subBreeds = json.message[breed];
+                        select.appendChild((() => {
+                            switch (subBreeds.length) {
+                            case 0:
+                                return document.createElement('option').
+                                    assign({
+                                        value: breed,
+                                        innerText: breed
+                                    }).setAttr({
+                                        optionType: 'breed'
+                                    });
+                            case 1:
+                                return document.createElement('option').
+                                    assign({
+                                        value: breed,
+                                        innerText: breed + ' ' + subBreeds[0]
+                                    }).setAttr({
+                                        optionType: 'subBreed'
+                                    });
+                            default:
+                                const group = document.createElement('optgroup').
+                                      assign({
+                                          label: breed
+                                      });
+                                group.appendChild(document.createElement('option').
+                                                  assign({
+                                                      value: breed,
+                                                      innerText: 'any '+breed
+                                                  }).setAttr({
+                                                      optionType: 'breed'
+                                                  }));
+                                for (const subBreed of subBreeds) {
+                                    group.appendChild(document.createElement('option').
+                                                      assign({
+                                                          value: subBreed,
+                                                          innerText: subBreed
+                                                      }).setAttr({
+                                                          optionType: 'subBreed'
+                                                      }));
+                                }
+                                return group;
+                            }
+                        })());
+                    };
+                } 
+            });
+    })();
 
 })();
