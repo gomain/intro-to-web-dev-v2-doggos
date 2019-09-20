35b088184b205c3cc1804f669acc431c5cea84f9
Joe Gomain Hoyes <gomain@gmail.com>
2019-09-20 11:38:28 +0700

    Refactor build select list.

--- script/doggos.js
+++ script/doggos.js
@@ -1,28 +1,24 @@
 
 (function() {
     'use strict'
-    
+
     /* monkey patch Object */
     const assign = Symbol('assign');
     Object.prototype[assign] = function (dict) {
         return Object.assign(this,dict);
     };
-    /** returns [ { name: ... , value: ... }, ... ] **/
-    const getOwnProperties = Symbol('getOwnProperties');
-    Object.prototype[getOwnProperties] = function () {
-        return Object.getOwnPropertyNames(this).
-            map(name => ({
-                name: name,
-                value: this[name]
-            }));
+    /** returns { <<name>> : <<value>>, ... } **/
+    const keys = Symbol('keys');
+    Object.prototype[keys] = function () {
+        return Object.keys(this);
     };
 
     /* monkey patch HTMLElement */
     const setAttributes = Symbol('setAttributes');
     HTMLElement.prototype[setAttributes] = function (dict) {
 	if (dict instanceof Object) {
-            for (const prop of dict[getOwnProperties]()) {
-		this.setAttribute(prop.name,prop.value);
+            for (const attr of dict[keys]()) {
+		this.setAttribute(attr,dict[attr]);
             }
 	}
         return this;
@@ -39,19 +35,19 @@
     const getElem = function (query) {
 	return document.querySelector(query);
     };
-    
+
     /* dict: {
-         type: 'string',
-         props: { ... },
-         attrs: { ... }
+       type: 'string',
+       props: { ... },
+       attrs: { ... }
        } */
     const elem = function (dict) {
         return document.createElement(dict.type)[assign](dict.props)[setAttributes](dict.attrs);
     };
-    
+
     /* dict: {
-         props: { ... },
-         attrs: { ... }
+       props: { ... },
+       attrs: { ... }
        } */
     const img = function (dict) {
         return elem({ type: 'img' }[assign](dict));
@@ -59,12 +55,12 @@
     const option = function (dict) {
         return elem({ type: 'option' }[assign](dict));
     };
-    
+
     /* dict : {
-         props: { ... },
-         attrs: { ... },
-         options: [ { ... }, ... ]
-       } */       
+       props: { ... },
+       attrs: { ... },
+       options: [ { ... }, ... ]
+       } */
     const optionGroup = function (dict) {
         return elem({ type: 'optGroup' }[assign]({ props: dict.props, attrs: dict.attrs }))[
             appendChildren](dict.options instanceof Array ?
@@ -72,157 +68,127 @@
                             undefined );
     };
     /* dict: {
-         props: { name: value, ... },
-         attrs: { name: value, ... },
-         options: [ { option }, ... }
+       props: { name: value, ... },
+       attrs: { name: value, ... },
+       options: [ { option }, ... }
        } */
     const select = function(dict) {
         return elem({ type: 'select'}[assign]({ props: dict.props, attrs: dict.attrs }))[
             appendChildren](dict.options instanceof Array ?
                             dict.options.map(opt => opt.options instanceof Array ?
-                                             optionGroup(opt) : option(opt)) :
+                                                  optionGroup(opt) : option(opt)) :
                             undefined);
     };
-    
-    /* test DOM helpers 
-    getElem('#test-select').appendChild(select({
-        options: [
-            {
-                props: {
-                    value: 'option-1-value',
-                    label: 'option-1-label',
-                    innerText: 'option-1'
-                },
-                attrs: {
-                    optionType: 'level-1'
-                }
-            },{
-                props: {
-                    value: 'option-2-value',
-                    label: 'option-2-label',
-                    innerText: 'option-2'
-                },
-                attrs: {
-                    optionType: 'level-1'
-                }
-            },{
-                props: {
-                    label: 'option-3'
-                },
-                options: [{
-                    props: {
-                        value: 'option-3.1-value',
-                        label: 'option-3.1-label',
-                        innerText: 'option-3.1'
-                    },
-                    attrs: {
-                        optionType: 'level-2'
-                    }
-                },{
-                    props: {
-                        value: 'option-3.2-value',
-                        label: 'option-3.2-label',
-                        innerText: 'option-3.2'
-                    },
-                    attrs: {
-                        optionType: 'level-2'
-                    }
-                }]
-            },{
-                props: {
-                    value: 'option-4-value',
-                    label: 'option-4-label',
-                    innerText: 'option-4'
-                },
-                attrs: {
-                    optionType: 'level-1'
-                }
-            }]
-    }));
-    */
-    
+
     const DOGURL = 'https://dog.ceo/api/breeds/image/random';
 
     const addDogButton = getElem('button#add-dog');
     addDogButton.addEventListener('click',() => {
         fetch(DOGURL).
-            then(response => response.json()).
+         then(response => response.json()).
             then(json => {
                 getElem('#doggo-gallery').
-                    appendChild(img({
-                        props: {
-                            src : json.message,
-                            alt : 'Cute doggo!'
-                        }
-                    }));
-                
+                                    appendChild(img({
+                                        props: {
+                                            src : json.message,
+                                            alt : 'Cute doggo!'
+                                        }
+                                    }));
+
             }).
 	    then(() => addDogButton.scrollIntoView());
     });
-    
+
     addDogButton.focus();
-    
+
     /* build breed select list */
     (function () {
+
+        function selectOptions(dict) {
+            return dict[keys]().map(breed => {
+                const breeds = dict[breed];
+                return breeds instanceof Array ?
+                       (breeds.length === 0 ?
+                        breedOption(breed) :
+                        breedOptionGroup(breed,breeds)):
+                       undefined
+            });
+        };
+
+        function breedOption(breed) {
+            return {
+                props: {
+                    label: breed,
+                    value: breed,
+                    innerText: breed
+                },
+                attrs: {
+                    optionType: 'breed',
+                    breed: breed
+                }
+            };
+        };
+
+        function breedOptionGroup(breed,breeds) {
+            return {
+                props: {
+                    label: breed
+                },
+                options: [{
+                    props: {
+                        label: breed,
+                        value: breed,
+                        innerText: 'any '+breed
+                    },
+                    attrs: {
+                        optionType: 'breed',
+                        breed: breed
+                    }
+                }].concat(breeds.map(subBreedOption.bind(null,breed)))
+            };
+        };
+
+        function subBreedOption (breed,subBreed) {
+            return {
+                props: {
+                    label: subBreed,
+                    value: subBreed,
+                    innerText: subBreed
+                },
+                attrs: {
+                    optionType: 'sub-breed',
+                    breed: breed,
+                    subBreed: subBreed
+                }
+            };
+        };
+
         const breedListUrl = 'https://dog.ceo/api/breeds/list/all';
-        fetch(breedListUrl).
-            then(response => response.json()).
-            then(json => {
+        const pickADoggo = {
+            props: {
+                disabled: true,
+                selected: true,
+                innerText: 'Pick a doggo...'
+            }
+        };
+        const anyDoggo = {
+            props: {
+                label: 'any',
+                value: 'any',
+                innerText: 'any doggo'
+            },
+            attrs: {
+                optionType: 'random'
+            }
+        };
+
+        fetch(breedListUrl)
+            .then(response => response.json())
+            .then(json => {
                 console.log(json);
                 getElem('#breed-list').appendChild(select({
-                    options: [{
-                        props: {
-                            disabled: true,
-                            selected: true,
-                            innerText: 'Pick a doggo...'
-                        }
-                    },{
-                        props: {
-                            label: 'any',
-                            value: 'any',
-                            innerText: 'any doggo'
-                        },
-                        attrs: {
-                            optionType: 'random'
-                        }
-                    }].concat(json.message[getOwnProperties]().map( breed => (
-                        breed.value.length ? {
-                            props: {
-                                label: breed.name
-                            },
-                            options: [{
-                                props: {
-                                    label: breed.name,
-                                    value: breed.name,
-                                    innerText: 'any '+breed.name
-                                },
-                                attrs: {
-                                    optionType: 'breed',
-                                    breed: breed.name
-                                }
-                            }].concat(breed.value.map(subBreed => ({
-                                props: {
-                                    label: subBreed,
-                                    value: subBreed,
-                                    innerText: subBreed
-                                },
-                                attrs: {
-                                    optionType: 'sub-breed',
-                                    breed: breed.name,
-                                    subBreed: subBreed
-                                }
-                            })))
-                        } : {
-                            props: {
-                                label: breed.name,
-                                value: breed.name,
-                                innerText: breed.name
-                            },
-                            attrs: {
-                                optionType: 'breed',
-                                breed: breed.name
-                            }
-                        })))
+                    options: [pickADoggo,anyDoggo]
+                        .concat(selectOptions(json.message))
                 }));
             });
     })();
